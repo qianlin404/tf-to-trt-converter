09810a58cec62c725d7e9501afce4e9800ecefa5
Qianlin Liang <qianlin.liang@qq.com>
2019-07-22 16:48:54 -0400

    test

--- utils/model.py
+++ utils/model.py
@@ -241,6 +241,9 @@ def ssd_mobilenet_v1_unsupported_nodes_to_plugin_nodes(ssd_graph, input_shape):
     Tensorflow operations that are not supported by TensorRT's UffParser
     and replacing them with custom layer plugin nodes.
 
+    Note: This specific implementation works only for
+    ssd_mobilenet_v2_coco_2018_03_29 network.
+
     Args:
         ssd_graph (gs.DynamicGraph): graph to convert
         input_shape: input shape in CHW format
@@ -255,17 +258,17 @@ def ssd_mobilenet_v1_unsupported_nodes_to_plugin_nodes(ssd_graph, input_shape):
     channels, height, width = input_shape
 
     Input = gs.create_plugin_node(name="Input",
-        op="Placeholder",
-        dtype=tf.float32,
-        shape=[1, channels, height, width])
+                                  op="Placeholder",
+                                  dtype=tf.float32,
+                                  shape=[1, channels, height, width])
     PriorBox = gs.create_plugin_node(name="GridAnchor", op="GridAnchor_TRT",
-        minSize=0.2,
-        maxSize=0.95,
-        aspectRatios=[1.0, 2.0, 0.5, 3.0, 0.33],
-        variance=[0.1,0.1,0.2,0.2],
-        featureMapShapes=[19, 10, 5, 3, 2, 1],
-        numLayers=6
-    )
+                                     minSize=0.2,
+                                     maxSize=0.95,
+                                     aspectRatios=[1.0, 2.0, 0.5, 3.0, 0.33],
+                                     variance=[0.1, 0.1, 0.2, 0.2],
+                                     featureMapShapes=[19, 10, 5, 3, 2, 1],
+                                     numLayers=6
+                                     )
     NMS = gs.create_plugin_node(
         name="NMS",
         op="NMS_TRT",
@@ -277,7 +280,7 @@ def ssd_mobilenet_v1_unsupported_nodes_to_plugin_nodes(ssd_graph, input_shape):
         topK=100,
         keepTopK=100,
         numClasses=91,
-        inputOrder=[0, 2, 1],
+        inputOrder=[1, 0, 2],
         confSigmoid=1,
         isNormalized=1
     )
@@ -304,14 +307,12 @@ def ssd_mobilenet_v1_unsupported_nodes_to_plugin_nodes(ssd_graph, input_shape):
 
     # Create a mapping of namespace names -> plugin nodes.
     namespace_plugin_map = {
-        "MultipleGridAnchorGenerator": PriorBox,
+        "MultipleGridAnchorGenerator": concat_priorbox,
         "Postprocessor": NMS,
         "Preprocessor/map": Input,
         "ToFloat": Input,
         # "image_tensor": Input,
-        "Preprocessor/ResizeImage": Input,
-        "Preprocessor/stack": Input,
-        "Preprocessor/unstack": Input,
+        "strided_slice_6": PriorBox,
         "Concatenate": concat_priorbox,
         "concat": concat_box_loc,
         "concat_1": concat_box_conf
@@ -325,6 +326,9 @@ def ssd_mobilenet_v1_unsupported_nodes_to_plugin_nodes(ssd_graph, input_shape):
     # If remove_exclusive_dependencies is True, the whole graph will be removed!
     ssd_graph.remove(ssd_graph.graph_outputs, remove_exclusive_dependencies=False)
     # Disconnect the Input node from NMS, as it expects to have only 3 inputs.
+
+    death_list = ["Shape_6", "strided_slice_7"]
+    ssd_graph.remove(ssd_graph.find_nodes_by_path(death_list))
 
     to_remove_nodes = ["Input", "Preprocessor/stack_1"]
     for node in to_remove_nodes:
